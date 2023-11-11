e75feb98e7ee3470ff49f070427f8ce42e7ae1f7
garbageowner <gbowner.github@gmail.com>
2023-11-11 16:35:04 +0300

    GRB-14 cover sale contract with tests

---
+++ test/TestHelper.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "src/test/ChainLinkPriceFeedMock.sol";
+import "src/GarbageSale.sol";
+import "lib/forge-std/src/Test.sol";
+
+contract GarbageSaleHarness is GarbageSale {
+    constructor(
+        address _priceFeed,
+        uint256 _usdPrice,
+        uint256 _presaleLimit
+    ) GarbageSale(_priceFeed, _usdPrice, _presaleLimit) {}
+
+    function setSaleLimitHarness(uint256 _saleLimit) public {
+        saleLimit = _saleLimit;
+    }
+}
+
+abstract contract TestHelper is Test {
+    ChainLinkPriceFeedMock public priceFeed;
+    GarbageSaleHarness public saleContract;
+
+    uint256 public tokenPrice = 20_000;
+    uint256 public saleLimit = 50_000_000;
+
+    address public owner = address(12345);
+
+    error ZeroPriceFeedAddress();
+    error WrongOracleData();
+    error TooLowValue();
+    error PerWalletLimitExceeded(uint256 remainingLimit);
+    error PresaleLimitExceeded(uint256 remainingLimit);
+    error NotEnoughEthOnContract();
+    error EthSendingFailed();
+
+    function setUp() public virtual {
+        priceFeed = new ChainLinkPriceFeedMock();
+        vm.warp(1 days);
+    }
+}
