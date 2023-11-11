36405dff0020af53c528356dc1fa7c3bec9598d6
garbageowner <gbowner.github@gmail.com>
2023-11-11 17:19:27 +0300

    GRB-14 create deployment script for testnet

---
+++ script/GarbageSaleDeployTestnet.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "src/test/GarbageSaleTestnet.sol";
+import "src/test/ChainLinkPriceFeedMock.sol";
+import "lib/forge-std/src/Script.sol";
+
+contract GarbageSaleDeployTestnetScript is Script {
+    uint256 public tokenPrice = 20_000;
+    uint256 public saleLimit = 50_000_000;
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        require(block.chainid == 11155111, "Should be deployed only on Sepolia");
+
+        address priceFeed = address(new ChainLinkPriceFeedMock());
+
+        new GarbageSaleTestnet(priceFeed, tokenPrice, saleLimit);
+
+        vm.stopBroadcast();
+    }
+}
